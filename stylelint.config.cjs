553389709d39c3a5d93fc0784f1c7a213337bdd9
Julien Veyssier <julien-nc@posteo.net>
2026-06-22 15:25:25 +0200

    add missing stylelint config
    Signed-off-by: Julien Veyssier <julien-nc@posteo.net>

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -1,6 +1,33 @@
-// SPDX-FileCopyrightText: 2026 Nextcloud GmbH and Nextcloud contributors
-// SPDX-License-Identifier: AGPL-3.0-or-later
+/**
+ * SPDX-FileCopyrightText: 2024 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
 
 module.exports = {
 	extends: 'stylelint-config-recommended-vue',
+	rules: {
+		'selector-type-no-unknown': null,
+		'rule-empty-line-before': [
+			'always',
+			{
+				ignore: ['after-comment', 'inside-block'],
+			},
+		],
+		'declaration-empty-line-before': [
+			'never',
+			{
+				ignore: ['after-declaration'],
+			},
+		],
+		'comment-empty-line-before': null,
+		'selector-type-case': null,
+		'no-descending-specificity': null,
+		'selector-pseudo-element-no-unknown': [
+			true,
+			{
+				ignorePseudoElements: ['v-deep'],
+			},
+		],
+	},
+	plugins: ['stylelint-scss'],
 }
